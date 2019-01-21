5cb8475c0b222e8bccc4078a3a62a65466886e2b
sh7ning <sh7ning@qq.com>
2019-01-21 11:07:00 +0800

    增加env获取辅助方法

--- src/Helper.php
+++ src/Helper.php
@@ -12,4 +12,37 @@ class Helper
 
         die(1);
     }
+
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public static function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+                return true;
+            case 'false':
+                return false;
+            case 'empty':
+                return '';
+            case 'null':
+                return;
+        }
+
+        if (($valueLength = strlen($value)) > 1 && $value[0] === '"' && $value[$valueLength - 1] === '"') {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
 }
